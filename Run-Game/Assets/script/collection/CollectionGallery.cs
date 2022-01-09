abd25f3b5122fe3bbd65afc07e46068c2f7f9500
k.ryou <58724780+ryouhei-kanezawa@users.noreply.github.com>
2022-01-10 04:45:54 +0900

    220109

--- Run-Game/Assets/script/collection/CollectionGallery.cs
+++ Run-Game/Assets/script/collection/CollectionGallery.cs
@@ -4,56 +4,56 @@ using UnityEngine;
 
 public class CollectionGallery
 {
-	public static bool get_character1 = false;
-	public static bool get_character2 = false;
-	public static bool get_character3 = false;
-	public static bool get_character4 = false;
-	public static bool get_character5 = false;
-	public static bool get_character6 = false;
-	public static bool get_character7 = false;
-	public static bool get_character8 = false;
-	public static bool get_character9 = false;
-	public static bool get_character10 = false;
-	public static bool get_character11 = false;
-	public static bool get_character12 = false;
-	public static bool get_character13 = false;
-	public static bool get_character14 = false;
-	public static bool get_character15 = false;
-	public static bool get_character16 = false;
-	public static bool get_character17 = false;
-	public static bool get_character18 = false;
-	public static bool get_character19 = false;
-	public static bool get_character20 = false;
-	public static bool get_character21 = false;
-	public static bool get_character22 = false;
-	public static bool get_character23 = false;
-	public static bool get_character24 = false;
-	public static bool get_character25 = false;
-	public static bool get_character26 = false;
-	public static bool get_character27 = false;
-	public static bool get_character28 = false;
-	public static bool get_character29 = false;
-	public static bool get_character30 = false;
-	public static bool get_character31 = false;
-	public static bool get_character32 = false;
-	public static bool get_character33 = false;
-	public static bool get_character34 = false;
-	public static bool get_character35 = false;
-	public static bool get_character36 = false;
-	public static bool get_character37 = false;
-	public static bool get_character38 = false;
-	public static bool get_character39 = false;
-	public static bool get_character40 = false;
-	public static bool get_character41 = false;
-	public static bool get_character42 = false;
-	public static bool get_character43 = false;
-	public static bool get_character44 = false;
-	public static bool get_character45 = false;
-	public static bool get_character46 = false;
-	public static bool get_character47 = false;
-	public static bool get_character48 = false;
-	public static bool get_character49 = false;
-	public static bool get_character50 = false;
+	public bool get_character1 = false;
+	public bool get_character2 = false;
+	public bool get_character3 = false;
+	public bool get_character4 = false;
+	public bool get_character5 = false;
+	public bool get_character6 = false;
+	public bool get_character7 = false;
+	public bool get_character8 = false;
+	public bool get_character9 = false;
+	public bool get_character10 = false;
+	public bool get_character11 = false;
+	public bool get_character12 = false;
+	public bool get_character13 = false;
+	public bool get_character14 = false;
+	public bool get_character15 = false;
+	public bool get_character16 = false;
+	public bool get_character17 = false;
+	public bool get_character18 = false;
+	public bool get_character19 = false;
+	public bool get_character20 = false;
+	public bool get_character21 = false;
+	public bool get_character22 = false;
+	public bool get_character23 = false;
+	public bool get_character24 = false;
+	public bool get_character25 = false;
+	public bool get_character26 = false;
+	public bool get_character27 = false;
+	public bool get_character28 = false;
+	public bool get_character29 = false;
+	public bool get_character30 = false;
+	public bool get_character31 = false;
+	public bool get_character32 = false;
+	public bool get_character33 = false;
+	public bool get_character34 = false;
+	public bool get_character35 = false;
+	public bool get_character36 = false;
+	public bool get_character37 = false;
+	public bool get_character38 = false;
+	public bool get_character39 = false;
+	public bool get_character40 = false;
+	public bool get_character41 = false;
+	public bool get_character42 = false;
+	public bool get_character43 = false;
+	public bool get_character44 = false;
+	public bool get_character45 = false;
+	public bool get_character46 = false;
+	public bool get_character47 = false;
+	public bool get_character48 = false;
+	public bool get_character49 = false;
+	public bool get_character50 = false;
 
 	public void OpenIllust(distinction _dis)
 	{
@@ -184,6 +184,115 @@ public class CollectionGallery
 				break;
 			default:
 				break;
+		}
+	}
+
+	public bool GetGet_character(int num)
+	{
+		switch (num)
+		{
+			case 1:
+				return get_character1;
+			case 2:
+				return get_character2;
+			case 3:
+				return get_character3;
+			case 4:
+				return get_character4;
+			case 5:
+				return get_character5;
+			case 6:
+				return get_character6;
+			case 7:
+				return get_character7;
+			case 8:
+				return get_character8;
+			case 9:
+				return get_character9;
+			case 10:
+				return get_character10;
+			case 11:
+				return get_character11;
+			case 12:
+				return get_character12;
+			case 13:
+				return get_character13;
+			case 14:
+				return get_character14;
+			case 15:
+				return get_character15;
+			case 16:
+				return get_character16;
+			case 17:
+				return get_character17;
+			case 18:
+				return get_character18;
+			case 19:
+				return get_character19;
+			case 20:
+				return get_character20;
+			case 21:
+				return get_character21;
+			case 22:
+				return get_character22;
+			case 23:
+				return get_character23;
+			case 24:
+				return get_character24;
+			case 25:
+				return get_character25;
+			case 26:
+				return get_character26;
+			case 27:
+				return get_character27;
+			case 28:
+				return get_character28;
+			case 29:
+				return get_character29;
+			case 30:
+				return get_character30;
+			case 31:
+				return get_character31;
+			case 32:
+				return get_character32;
+			case 33:
+				return get_character33;
+			case 34:
+				return get_character34;
+			case 35:
+				return get_character35;
+			case 36:
+				return get_character36;
+			case 37:
+				return get_character37;
+			case 38:
+				return get_character38;
+			case 39:
+				return get_character39;
+			case 40:
+				return get_character40;
+			case 41:
+				return get_character41;
+			case 42:
+				return get_character42;
+			case 43:
+				return get_character43;
+			case 44:
+				return get_character44;
+			case 45:
+				return get_character45;
+			case 46:
+				return get_character46;
+			case 47:
+				return get_character47;
+			case 48:
+				return get_character48;
+			case 49:
+				return get_character49;
+			case 50:
+				return get_character50;
+			default:
+				return false;
 		}
 	}
 }
